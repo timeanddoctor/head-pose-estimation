d99a53613ddac19c0837f9519f0a54400a0dd698
yinguobing <yinguobing@gmail.com>
2018-01-16 10:45:32 +0800

    add code to debug local dense flow

--- estimate_head_pose.py
+++ estimate_head_pose.py
@@ -12,7 +12,35 @@ import mark_detector
 # import optical_flow_tracker
 from stabilizer import Stabilizer
 import pose_estimator
+# Get left eye left corner.
+# local_x = int(marks[36][0])
+# local_y = int(marks[36][1])
 
+# # Extract local area.
+# local_box = [local_x - 12, local_y - 12,
+#              local_x + 12, local_y + 12]
+
+# if mark_detector.box_in_image(local_box, frame):
+#     local_img = frame_cnn[local_box[1]:local_box[3],
+#                           local_box[0]:local_box[2]]
+#     local_img = cv2.resize(
+#         local_img, (512, 512), interpolation=cv2.INTER_AREA)
+#     cv2.imshow('local', local_img)
+
+# # Get left eye left corner.
+# local_x = int(marks_refined[2][0])
+# local_y = int(marks_refined[2][1])
+
+# # Extract local area.
+# local_box = [local_x - 12, local_y - 12,
+#              local_x + 12, local_y + 12]
+
+# if mark_detector.box_in_image(local_box, frame):
+#     local_img = frame_cnn[local_box[1]:local_box[3],
+#                           local_box[0]:local_box[2]]
+#     local_img = cv2.resize(
+#         local_img, (512, 512), interpolation=cv2.INTER_AREA)
+#     cv2.imshow('local_refined', local_img)
 INPUT_SIZE = 128
 
 
@@ -36,12 +64,25 @@ def refine_mark_by_flow(frame, mark, img_prev):
                                                   local_img_gray,
                                                   None,
                                                   0.5, 3, 15, 3, 5, 1.2, 0)
+        shift_x = 0
+        shift_y = 0
+        for row in range(4, 24, 4):
+            for col in range(4, 24, 4):
+                d_x, d_y = dense_flow[row, col]
+                shift_x += d_x
+                shift_y += d_y
+        shift_x /= 25
+        shift_y /= 25
+        # print(int(shift_x), int(shift_y))
 
-        temp = np.sum(dense_flow, axis=0)
-        shift_x, shift_y = np.sum(temp, axis=0).flatten() / \
-            (dense_flow.shape[0] * dense_flow.shape[1])
+        # for row in range(0, 24, 6):
+        #     for col in range(0, 24, 6):
+        #         cv2.circle(local_img, (col + int(shift_x), row +
+        #                                int(shift_y)), 1, (255, 255, 0), -1)
+        # local_img = cv2.resize(local_img, (512, 512), interpolation=cv2.INTER_AREA)
+        # cv2.imshow('flow', local_img)
 
-        return [mark[0] + shift_x, mark[1] + shift_y], local_img_gray
+        return [mark[0] + int(shift_x), mark[1] + int(shift_y)], local_img_gray
     return None, None
 
 
@@ -53,7 +94,7 @@ def main():
 
     # Introduce point stabilizers for landmarks.
     point_stabilizers = [Stabilizer(
-        cov_process=0.001, cov_measure=0.1) for _ in range(68)]
+        cov_process=0.01, cov_measure=0.1) for _ in range(68)]
 
     # Introduce scalar stabilizers for pose.
     pose_stabilizers = [Stabilizer(
@@ -170,15 +211,41 @@ def main():
                     marks_refined[idx] = fine_mark
 
             # Uncomment the following line to show raw marks.
-            mark_detector.draw_marks(frame_cnn, marks, color=(255, 255, 255))
-            mark_detector.draw_marks(
-                frame_cnn, marks_refined, color=(0, 255, 0))
-            mark_detector.draw_marks(
-                frame_cnn, stabile_marks, color=(255, 0, 0))
+            # mark_detector.draw_marks(frame_cnn, marks, color=(255, 255, 255))
+            # mark_detector.draw_marks(
+            #     frame_cnn, marks_refined, color=(0, 255, 0))
+            # mark_detector.draw_marks(
+            #     frame_cnn, stabile_marks, color=(255, 0, 0))
 
             # Get left eye left corner.
-            local_x = int(stabile_marks[36][0])
-            local_y = int(stabile_marks[36][1])
+            local_x = int(marks[36][0])
+            local_y = int(marks[36][1])
+
+            # Extract local area.
+            local_box = [local_x - 12, local_y - 12,
+                         local_x + 12, local_y + 12]
+
+            if mark_detector.box_in_image(local_box, frame):
+                local_img = frame_cnn[local_box[1]:local_box[3],
+                                      local_box[0]:local_box[2]]
+
+            local_img = cv2.resize(
+                local_img, (480, 480), interpolation=cv2.INTER_AREA)
+
+            # for row in range(4, 24, 4):
+            #     for col in range(4, 24, 4):
+            #         shift_x, shift_y = flow[row, col]
+            #         fx = int(shift_x * 20)
+            #         fy = int(shift_y * 20)
+            #         print(fx, fy)
+            #         cv2.circle(local_img,
+            #                    (col * 20 + fx, row * 20 + fy),
+            #                    20, (0, 128, 0))
+            cv2.imshow('local', local_img)
+
+            # Get left eye left corner.
+            local_x = int(marks_refined[2][0])
+            local_y = int(marks_refined[2][1])
 
             # Extract local area.
             local_box = [local_x - 12, local_y - 12,
@@ -188,8 +255,8 @@ def main():
                 local_img = frame_cnn[local_box[1]:local_box[3],
                                       local_box[0]:local_box[2]]
                 local_img = cv2.resize(
-                    local_img, (512, 512), interpolation=cv2.INTER_AREA)
-                cv2.imshow('local', local_img)
+                    local_img, (480, 480), interpolation=cv2.INTER_AREA)
+                cv2.imshow('local_refined', local_img)
 
             # All kind of detections face a common issue: jitter. Usually this is
             # solved by kinds of estimators, like partical filter or Kalman filter, etc.
@@ -230,32 +297,39 @@ def main():
             #     frame_cnn, stabile_marks, color=(255, 0, 0))
 
             # Try pose estimation
-            pose_marks = pose_estimator.get_pose_marks(marks)
-            pose_marks = marks_refined
+            pose_marks = pose_estimator.get_pose_marks(stabile_marks)
+            # pose_marks = marks_refined
             pose_marks = np.array(pose_marks, dtype=np.float32)
 
             pose = pose_estimator.solve_pose(pose_marks)
 
+            # Solve pose by 68 points
+            # pose_marks = np.array(stabile_marks, dtype=np.float32)
+            # pose = pose_estimator.solve_pose_by_68_points(pose_marks)
+
             # Stabilize the pose.
-            stabile_pose = []
-            pose_np = np.array(pose).flatten()
-            for value, ps_stb in zip(pose_np, pose_stabilizers):
-                ps_stb.update([value])
-                stabile_pose.append(ps_stb.state[0])
-            stabile_pose = np.reshape(stabile_pose, (-1, 3))
+            # stabile_pose = []
+            # pose_np = np.array(pose).flatten()
+            # for value, ps_stb in zip(pose_np, pose_stabilizers):
+            #     ps_stb.update([value])
+            #     stabile_pose.append(ps_stb.state[0])
+            # stabile_pose = np.reshape(stabile_pose, (-1, 3))
 
             # Draw pose annotaion on frame.
-            # frame_cnn = pose_estimator.draw_annotation_box(
-            #     frame_cnn, pose[0], pose[1])
             frame_cnn = pose_estimator.draw_annotation_box(
-                frame_cnn, stabile_pose[0], stabile_pose[1], color=(0, 255, 0))
+                frame_cnn, pose[0], pose[1])
+            # frame_cnn = pose_estimator.draw_annotation_box(
+            #     frame_cnn, stabile_pose[0], stabile_pose[1], color=(0, 255, 0))
 
         # Show preview.
         cv2.imshow("Preview", frame_cnn)
 
         debug_img = frame_cnn[200:224, 300:324]
+
         debug_img = cv2.resize(
-            debug_img, (512, 512), interpolation=cv2.INTER_AREA)
+            debug_img, (480, 480), interpolation=cv2.INTER_AREA)
+        cv2.line(debug_img, (0, 240), (480, 240), (255, 255, 255), 1)
+        cv2.line(debug_img, (240, 0), (240, 480), (255, 255, 255), 1)
         cv2.imshow('debug', debug_img)
 
         if cv2.waitKey(10) == 27:
